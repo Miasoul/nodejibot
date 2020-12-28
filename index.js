39949900d51963210f3bfcdcdadd00b4900a6305
Miasoul <74416346+Miasoul@users.noreply.github.com>
2020-12-28 16:32:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client ();
 const token = process.env.token;
 bot.on('guildMemberAdd', member => {
-   let embed = new Discord.RichEmbed()
+   const embed = new Discord.MessageEmbed()
       .setTitle(`안녕하세요 ${member.displayName}님`) // 여기는 임베드에서 타이틀로 사용됩니다!
       .addField("테스트보러들어오신건가요? 아님 다른클랜으로 들어오신건가요?", "Miasoul#0811 로 친추 걸어주세요")
       member.send(embed)
